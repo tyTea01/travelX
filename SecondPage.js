a392933eb8c27c5144833bf8b92b767368fdc978
tyTea01 <60376024+tyTea01@users.noreply.github.com>
2022-10-20 07:52:09 -0700

    Add files via upload

---
+++ SecondPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+
+function SecondPage() {
+    let history = useHistory();
+    history.push('/')
+    return (
+        <div>
+            
+        </div>
+    )
+}
