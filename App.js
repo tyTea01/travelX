2cf5d9317a221ff41870b56b1ebcbdebbc6d17a3
tyTea01 <60376024+tyTea01@users.noreply.github.com>
2022-11-08 10:52:25 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './Header';
 import Home from './Home';
 import SecondPage from './SecondPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Header/>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
